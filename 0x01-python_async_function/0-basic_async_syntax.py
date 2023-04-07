82118c3ab1fc8cbfdd94f6fbd1648c36246172c5
Tebiremen <okojie.aitebiremen@gmail.com>
2023-04-07 08:23:47 +0000

    Task 0 Module

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,12 +2,12 @@
 '''The basics of task 0's module.
 '''
 import asyncio
-import random
+from random import uniform
 
 
-async def wait_random(max_delay: int = 10) -> float;
+async def wait_random(max_delay: int = 10) -> float:
     '''Waits for a random number of seconds.
     '''
-    wait_time = random.random() * maxx_delay
-    await asyncio.sleep(wait_time)
-    return wait_time
+    delay = uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
