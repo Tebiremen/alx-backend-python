c371f4edc40500f1525a08f40dfa33cfcccc4906
Tebiremen <okojie.aitebiremen@gmail.com>
2023-04-07 10:35:41 +0000

    Multiple coroutiens at the same time with async

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,8 +10,6 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     ''' Executes wait_random n times'''
-
-
     delay_list = []
     i = 0
 
