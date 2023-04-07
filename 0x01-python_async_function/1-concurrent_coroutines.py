cb587c6ba461858f6c051eccd532f50ca5c48b12
Tebiremen <okojie.aitebiremen@gmail.com>
2023-04-07 09:16:40 +0000

    Task 1 Module

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,9 +9,10 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 asyn def wait_n(n: int, max_delay: int) -> List[float]:
-    '''Test file for printing the correct output of the wait_n coroutine
+    '''Executing wait_random n times.
     '''
     delay_list = await asyncio.gather(
-            *tuple(map(lambda _: wait_random(max_delay), range(n)))
+            delay_list.append(await wait_random(max_delay))
+
     )
     return sorted(delay_list)
