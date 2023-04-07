eb134f64219a90eb3937b8a1b625a931263934cb
Tebiremen <okojie.aitebiremen@gmail.com>
2023-04-07 08:58:35 +0000

    Task 1 Module

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''executing multiple coroutines at the same time with async
+'''
+import asyncio
+from typing import list
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+asyn def wait_n(n: int, max_delay: int) -> List[float]:
+    '''Test file for printing the correct output of the wait_n coroutine
+    '''
+    wait_times = await asyncio.gather(
+            *tuple(map(lambda _: wait_random(max_delay), range(n)))
+    )
+    return sorted(wait_times)
